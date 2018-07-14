ffeda9391aa47fda33dc3d7156d1e2d6af82a327
andrewgwallace <andrew.g.wallace@gmail.com>
2018-07-14 13:39:31 -0400

    Have demo markers show on App.js file

---
+++ client/src/components/app/App.js
@@ -0,0 +1,202 @@
+// import React from "react"
+// import { compose, withProps } from "recompose"
+// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+// const MyMapComponent = compose(
+//   withProps({
+//     googleMapURL:
+//       "https://maps.googleapis.com/maps/api/js?key=AIzaSyA3HUPGnMXmJP39ubMsFBVHjX1NNGwjY9A&v=3.exp&libraries=geometry,drawing,places",
+//     loadingElement: <div style={{ height: `100%` }} />,
+//     containerElement: <div style={{ height: `400px` }} />,
+//     mapElement: <div style={{ height: `100%` }} />
+//   }),
+//   withScriptjs,
+//   withGoogleMap
+// )(props => (
+//   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+//     {props.isMarkerShown && (
+//       <Marker
+//         position={{ lat: -34.397, lng: 150.644 }}
+//         onClick={props.onMarkerClick}
+//       />
+//     )}
+//   </GoogleMap>
+// ));
+
+// ~~~~~~~~~~~~
+import React from "react"
+const fetch = require("isomorphic-fetch");
+const { compose, withProps, withHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} = require("react-google-maps");
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
+
+const MapWithAMarkerClusterer = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA3HUPGnMXmJP39ubMsFBVHjX1NNGwjY9A&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withHandlers({
+    onMarkerClustererClick: () => (markerClusterer) => {
+      const clickedMarkers = markerClusterer.getMarkers()
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+      console.log(clickedMarkers)
+    },
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={3}
+    defaultCenter={{ lat: 25.0391667, lng: 121.525 }}
+  >
+    <MarkerClusterer
+      onClick={props.onMarkerClustererClick}
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.markers.map(marker => (
+        <Marker
+          key={marker.photo_id}
+          position={{ lat: marker.latitude, lng: marker.longitude }}
+        />
+      ))}
+    </MarkerClusterer>
+  </GoogleMap>
+);
+
+class DemoApp extends React.PureComponent {
+  componentWillMount() {
+    this.setState({ markers: [] })
+  }
+
+  componentDidMount() {
+    const url = [
+      // Length issue
+      `https://gist.githubusercontent.com`,
+      `/farrrr/dfda7dd7fccfec5474d3`,
+      `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
+    ].join("")
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ markers: data.photos });
+      });
+  }
+
+  render() {
+    return (
+      <MapWithAMarkerClusterer markers={this.state.markers} />
+    )
+  }
+}
+
+<DemoApp />
+
+export default DemoApp;
+
+
+
+
+
+
+
+
+
+
+// ~~~~~~~~~~~~~~
+
+// class MyFancyComponent extends React.PureComponent {
+//   state = {
+//     isMarkerShown: false,
+//   }
+
+//   componentDidMount() {
+//     this.delayedShowMarker()
+//   }
+
+//   delayedShowMarker = () => {
+//     setTimeout(() => {
+//       this.setState({ isMarkerShown: true })
+//     }, 3000)
+//   }
+
+//   handleMarkerClick = () => {
+//     this.setState({ isMarkerShown: false })
+//     this.delayedShowMarker()
+//   }
+
+//   render() {
+//     return (
+//       <MyMapComponent
+//         isMarkerShown={this.state.isMarkerShown}
+//         onMarkerClick={this.handleMarkerClick}
+//       />
+//     )
+//   }
+// }
+
+// export default MyFancyComponent
+
+
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import { compose, withProps } from "recompose"
+// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+// const MyMapComponent = compose(
+//   withProps({
+//     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+//     loadingElement: <div style={{ height: `100%` }} />,
+//     containerElement: <div style={{ height: `400px` }} />,
+//     mapElement: <div style={{ height: `100%` }} />,
+//   }),
+//   withScriptjs,
+//   withGoogleMap
+// )((props) =>
+//   <GoogleMap
+//     defaultZoom={8}
+//     defaultCenter={{ lat: -34.397, lng: 150.644 }}
+//   >
+//     {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+//   </GoogleMap>
+// ))
+
+// <MyMapComponent isMarkerShown />
+
+
+
+// class App extends Component {
+//   state = {
+//     loading: true,
+//   }
+
+//   componentDidMount = async () => {
+//     const response = await fetch('/api/ping')
+//     const json = await response.json()
+//     if (json.message) this.setState({ loading: false})
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         {
+//           !this.state.loading &&
+//           <h1>You've connected to the server!</h1>
+//         }
+//         </div>
+//     );
+//   }
+// }
+
+// export default App;
