04da4b151fd65505343bf9b3110f7f693094b096
andrewgwallace <andrew.g.wallace@gmail.com>
2018-07-14 13:51:02 -0400

    Add comments to understand file

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -33,35 +33,40 @@ const {
   GoogleMap,
   Marker,
 } = require("react-google-maps");
+
+// Required library to show the marker clusters
 const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
+// Using the compose method component from the recompse library, generate the the properties required for the map element to be displayed
 const MapWithAMarkerClusterer = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA3HUPGnMXmJP39ubMsFBVHjX1NNGwjY9A&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyA3HUPGnMXmJP39ubMsFBVHjX1NNGwjY9A&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />
   }),
+  // Handler functions that act in response to synthetic events occurring in React.
+  // DOCUMENTATION: https://recompose.docsforhumans.com/withhandlers.html These functions are passed as immmutable props (they don't change)
   withHandlers({
-    onMarkerClustererClick: () => (markerClusterer) => {
-      const clickedMarkers = markerClusterer.getMarkers()
-      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
-      console.log(clickedMarkers)
-    },
+    onMarkerClustererClick: () => markerClusterer => {
+      const clickedMarkers = markerClusterer.getMarkers();
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`);
+      console.log(clickedMarkers);
+    }
   }),
   withScriptjs,
   withGoogleMap
-)(props =>
-  <GoogleMap
-    defaultZoom={3}
-    defaultCenter={{ lat: 25.0391667, lng: 121.525 }}
-  >
+  // "props" here is a name simply given (could have been called anything) to identify the withProps that were provided for the Marker Cluster that pass the required info by Google Maps; the defaultZoom and defaultCenter.
+)(props => (
+  <GoogleMap defaultZoom={3} defaultCenter={{ lat: 25.0391667, lng: 121.525 }}>
     <MarkerClusterer
       onClick={props.onMarkerClustererClick}
       averageCenter
       enableRetinaIcons
       gridSize={60}
     >
+    {/* Map over each marker and set the key to the photo_id -- This will change to the performance_id, seting the lat and lng to the performance latitude and longitude */}
       {props.markers.map(marker => (
         <Marker
           key={marker.photo_id}
@@ -70,8 +75,9 @@ const MapWithAMarkerClusterer = compose(
       ))}
     </MarkerClusterer>
   </GoogleMap>
-);
+));
 
+// Establish the component to rendered passing in the state which contains the markers. `markers: data.photos` is an array; you'll pass the array of performances.
 class DemoApp extends React.PureComponent {
   componentWillMount() {
     this.setState({ markers: [] })
@@ -92,14 +98,13 @@ class DemoApp extends React.PureComponent {
       });
   }
 
+  // Render the state of the markers on the map. 
   render() {
     return (
       <MapWithAMarkerClusterer markers={this.state.markers} />
     )
   }
 }
-
-<DemoApp />
 
 export default DemoApp;
 
