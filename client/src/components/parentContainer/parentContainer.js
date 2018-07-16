6779146b2ff5b543f1d65e34c1346f5dfac2a324
andrewgwallace <andrew.g.wallace@gmail.com>
2018-07-16 11:10:45 -0400

    Clear parent container for master branch

---
+++ client/src/components/parentContainer/parentContainer.js
@@ -0,0 +1,9 @@
+// This container is the wrapper for the other compontents as it contains the primary state and methods.
+import React, { Component } from "react";
+
+class ParentContainer extends Component {
+
+
+}
+
+export default ParentContainer;
