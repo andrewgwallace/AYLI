b392e4d1cfb430405ddacedb58dd3e45b31bdf52
andrewgwallace <andrew.g.wallace@gmail.com>
2018-08-01 13:42:17 -0400

    Add seed file to production

---
+++ db/seeds/production/01_artists.js
@@ -0,0 +1,40 @@
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('artists').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('artists').insert([
+        {
+          id: "1",
+          twitter_id: "agwallace",
+          displayName: "Andrew Wallace",
+          email: "andrew.g.wallace@gmail.com",
+          profileImg: "https://pbs.twimg.com/profile_images/976960658020368384/PJ0N4Pty_normal.jpg",
+          instagram_id: "feigningfigure",
+          youtube_id: 'nanascho3',
+          approved: "2" //'2' = approved
+        },
+        {
+          id: "2",
+          twitter_id: "wuillymay",
+          displayName: "Wuilly Moises Arteaga",
+          profileImg: "https://pbs.twimg.com/profile_images/963421744458592256/HF85rgaR_normal.jpg",
+          email: "wuillymay@email.com",
+          instagram_id: "wuillymay",
+          youtube_id: '',
+          approved: "2"
+        },
+        {
+          id: "3",
+          twitter_id: "ayliapp",
+          displayName: "AYLIAPP",
+          profileImg: "https://pbs.twimg.com/profile_images/1019671193282985984/yI3sq-jQ_normal.jpg", 
+          email: "ayli@ayliapp.com",
+          instagram_id: "ayliapp",
+          youtube_id: '',
+          approved: "2"
+        }
+      ]);
+    });
+};
